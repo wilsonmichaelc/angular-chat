6efbe59c45a05e742a4062b0a59522bc2e31a754
Michael Wilson <wilson.michael.c@gmail.com>
2019-06-26 17:39:38 -0400

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ChatService } from './chat.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'chat';
+
+  public users: number = 0;
+  public message: string = '';
+  public messages: string[] = [];
+
+  constructor(private chatService: ChatService){
+
+  }
+
+  ngOnInit(){
+
+    this.chatService.receiveChat().subscribe((message: string) => {
+      console.log(message);
+      this.messages.push(message);
+    });
+
+    this.chatService.getUsers().subscribe((users: number) => {
+      console.log(users);
+      this.users = users;
+    });
+
+  }
+
+  addChat(){
+    this.messages.push(this.message);
+    this.chatService.sendChat(this.message);
+    this.message = '';
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,24 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { FormsModule } from '@angular/forms';
+
+import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
+import { ChatService } from './chat.service';
+
+const config: SocketIoConfig = { url: 'http://localhost:4001', options: {}};
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    SocketIoModule.forRoot(config)
   ],
-  providers: [],
+  providers: [ChatService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
